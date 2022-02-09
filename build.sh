02cbca5d1ad779187a9b36c832a4f7989d857de6
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-09 15:07:36 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dir=`dirname "$0"`
-cd "$dir"
-rm -rf project/obj
-lime rebuild . ios
-rm -rf project/obj
-rm -f extension-webview.zip
-zip -r extension-webview.zip extension dependencies assets haxelib.json include.xml project ndll android 
