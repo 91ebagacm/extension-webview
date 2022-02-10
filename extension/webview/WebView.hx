64b218b00bdf1c777b808d9cd1baf9191c7a369e
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-10 16:33:31 +0200

    Experimental Changes

--- extension/webview/WebView.hx
+++ extension/webview/WebView.hx
@@ -1,52 +1,52 @@
 package extension.webview;
 
+#if android
 import haxe.Json;
 import lime.system.JNI;
+#end
 	
 class WebView  
 {
-	private static var initialized :Bool = false;
-	private static var _open:String -> Void = null;
 	public static var onClose:Void->Void=null;
 	public static var onURLChanging:String->Void=null;
 
-	public static function open(url:String = null, floating:Bool = false, ?urlWhitelist:Array<String>, ?urlBlacklist:Array<String>, ?useWideViewPort:Bool = false, ?mediaPlaybackRequiresUserGesture:Bool = false):Void 
-        {
+	public static function open(url:String = null, floating:Bool = false, ?urlWhitelist:Array<String>, ?urlBlacklist:Array<String>, ?useWideViewPort:Bool = false, ?mediaPlaybackRequiresUserGesture:Bool = false):Void {
                 #if android
-		init();
+		_callbackFunc(new AndroidCallbackHelper());
+
 		if(urlWhitelist!=null) 
                         urlWhitelist.push(url);
-		if (urlWhitelist==null) {
+		if (urlWhitelist==null)
 			urlWhitelist = [];
-		}
-		if (urlBlacklist==null) {
+		if (urlBlacklist==null)
 			urlBlacklist = [];
-		}
-		var obj = {
-			url : url,
-			floating : floating,
-			urlWhitelist : urlWhitelist,
-			urlBlacklist : urlBlacklist,
-			useWideViewPort : useWideViewPort,
-			mediaPlaybackRequiresUserGesture : mediaPlaybackRequiresUserGesture
-		}
+
+		var obj = {url:url, floating:floating, urlWhitelist:urlWhitelist, urlBlacklist:urlBlacklist, useWideViewPort:useWideViewPort, mediaPlaybackRequiresUserGesture:mediaPlaybackRequiresUserGesture}
 		_open(Json.stringify(obj));
                 #end
 	}
 
-	private static function init():Void 
-        {
-                #if android
-		if(initialized == true) 
-                        return;
-		initialized = true;
-		try {
-		        _open = JNI.createStaticMethod("extensions/webview/WebViewExtension", "open", "(Ljava/lang/String;)V");
-			var _callbackFunc = JNI.createStaticMethod("extensions/webview/WebViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
-			_callbackFunc(new AndroidCallbackHelper());
-		} catch(e:Dynamic) {
-			trace("INIT Exception: "+e);
+        #if android
+        private static var _open = JNI.createStaticMethod("extensions/webview/WebViewExtension", "open", "(Ljava/lang/String;)V");	
+        private static var _callbackFunc = JNI.createStaticMethod("extensions/webview/WebViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
+        #end
+}
+
+class AndroidCallbackHelper {
+
+	public function new() {
+
+	}
+
+	public function onClose() {
+		if (WebView.onClose != null) {
+			WebView.onClose();
 		}
-                #end
-	}	
+	}
+
+	public function onURLChanging(url:String) {      
+		if (WebView.onURLChanging != null) {
+			WebView.onURLChanging(url);
+		}        
+	}
 }
