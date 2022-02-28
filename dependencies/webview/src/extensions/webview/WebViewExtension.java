a51f58c3d5bcc38be8e6653f6aca8c33ecbc67fb
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-28 08:59:13 +0200

    Update WebViewExtension.java

--- dependencies/webview/src/extensions/webview/WebViewExtension.java
+++ dependencies/webview/src/extensions/webview/WebViewExtension.java
@@ -14,8 +14,8 @@ public class WebViewExtension extends Extension {
 	public static final String EXTRA_HTML = "extensions.webviewex.EXTRA_HTML";
 	public static final String EXTRA_URL_WHITELIST = "extensions.webviewex.EXTRA_URL_WHITELIST";
 	public static final String EXTRA_URL_BLACKLIST = "extensions.webviewex.EXTRA_URL_BLACKLIST";
+	public static final String EXTRA_USE_HIDE_UI = "extensions.webviewex.EXTRA_USE_HIDE_UI";
 	public static final String EXTRA_USE_WIDE_PORT = "extensions.webviewex.EXTRA_USE_WIDE_PORT";
-	public static final String EXTRA_MEDIA_PLAYBACK_REQUIRES_USER_GESTURE = "extensions.webviewex.EXTRA_MEDIA_PLAYBACK_REQUIRES_USER_GESTURE";
 	public static boolean active = false;
 
 	public static HaxeObject callback;
@@ -37,12 +37,14 @@ public class WebViewExtension extends Extension {
 				urlBlacklist[i] = jsonUrlBlacklist.getString(i);
 			}
 
+			boolean hideui = obj.getBoolean("hideui");
 			boolean useWideViewPort = obj.getBoolean("useWideViewPort");
 
 			Intent intent = new Intent(mainActivity, WebViewActivity.class);
 			intent.putExtra(EXTRA_URL, url);
 			intent.putExtra(EXTRA_URL_WHITELIST, urlWhitelist);
 			intent.putExtra(EXTRA_URL_BLACKLIST, urlBlacklist);
+			intent.putExtra(EXTRA_USE_HIDE_UI, hideui);
 			intent.putExtra(EXTRA_USE_WIDE_PORT, useWideViewPort);
 
 			mainActivity.startActivity(intent);
@@ -58,10 +60,12 @@ public class WebViewExtension extends Extension {
 			JSONObject obj = new JSONObject(json);
 			String html = obj.getString("html");
 
+			boolean hideui = obj.getBoolean("hideui");
 			boolean useWideViewPort = obj.getBoolean("useWideViewPort");
 
 			Intent intent = new Intent(mainActivity, WebViewActivity.class);
 			intent.putExtra(EXTRA_HTML, html);
+			intent.putExtra(EXTRA_USE_HIDE_UI, hideui);
 			intent.putExtra(EXTRA_USE_WIDE_PORT, useWideViewPort);
 
 			mainActivity.startActivity(intent);
